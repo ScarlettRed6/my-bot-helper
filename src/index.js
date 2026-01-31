e0d3f02765e466920e1c046808fa86f0ac878cd5
ScarlettRed6 <jcaamrelo@gmail.com>
2026-01-31 16:38:59 +0800

    new imports for command handling

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import { Client, Events, GatewayIntentBits } from 'discord.js';
+import fs from 'fs';
+import path from 'path';
+import { Client, Collection, Events, GatewayIntentBits, MessageFlags } from 'discord.js';
 
 const token = process.env.DISCORD_TOKEN;
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once(Events.ClientReady, (readyClient) => {
-    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
+
+client.commands = new Collection();
+
 
 client.login(token);
