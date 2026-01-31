bf8fe41af7aa5aea737880ebe5e91fdbf37b0a17
ScarlettRed6 <jcaamrelo@gmail.com>
2026-01-31 17:09:50 +0800

    Dynamic retrieval of command files with file filtering so that only command files are processed.

--- src/index.js
+++ src/index.js
@@ -12,5 +12,23 @@ client.once(Events.ClientReady, (readyClient) => {
 
 client.commands = new Collection();
 
+//Setup for command file path, this helps construct the file path 
+const foldersPath = path.join(__dirname, "commands");
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ('data' in command && 'execute' in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+
+    }//End of ifelse statement
+  }//End of inner loop
+}//End of outer loop
 
 client.login(token);
