ada3c6c4fa8b237021163abea7f7bf56729fba40
ScarlettRed6 <jcaamrelo@gmail.com>
2026-02-01 19:13:44 +0800

    using the constructed instance of REST module to deploy the commands as well as adding a refresh when reloading commands

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -27,6 +27,17 @@ for (const folder of commandFolders) {
   }//End of inner loop
 }//End of outer loop
 
+const rest = new REST().setToken(token);
 
+(async () => {
+  try {
+    console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+    const data = await rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands });
+    console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+  } catch (error) {
+    console.log(error);
+  }
+})();
 
 
