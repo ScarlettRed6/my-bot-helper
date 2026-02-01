4fada21820f87467dbd3493649fc8c52debc744c
ScarlettRed6 <jcaamrelo@gmail.com>
2026-02-01 17:08:42 +0800

    Fixed: using commonjs modules mix up with es modules, fixed command files retrieval with import and pathToFileURL since i am using export default {} for my command files i had to use default keyword in destructured command import

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import path from 'path';
+import { pathToFileURL } from 'url';
 import { Client, Collection, Events, GatewayIntentBits, MessageFlags } from 'discord.js';
 
 const token = process.env.DISCORD_TOKEN;
@@ -13,7 +14,7 @@ client.once(Events.ClientReady, (readyClient) => {
 client.commands = new Collection();
 
 //Setup for command file path, this helps construct the file path 
-const foldersPath = path.join(__dirname, "commands");
+const foldersPath = path.join(import.meta.dirname, "commands");
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
@@ -21,7 +22,8 @@ for (const folder of commandFolders) {
   const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
   for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
+    //const command = require(filePath);
+    const { default: command } = await import(pathToFileURL(filePath).href);
     if ('data' in command && 'execute' in command) {
       client.commands.set(command.data.name, command);
     } else {
